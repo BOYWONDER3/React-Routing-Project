20f359c93446e62dd4e0557f6b3299943d541074
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-07-15 23:22:41 +0100

    third commit

---
+++ client/src/layouts/RootLayout.jsx
@@ -0,0 +1,27 @@
+import { Link, Outlet, ScrollRestoration } from "react-router-dom";
+
+export function RootLayout() {
+  return (
+    <>
+      <nav class="top-nav">
+        <div className="nav-text-large">My App</div>
+        <ul className="nav-list">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/todos">Todos</Link>
+          </li>
+        </ul>
+      </nav>
+      <ScrollRestoration />
+      <div className="container">
+        <Outlet />
+      </div>
+    </>
+  );
+  return <Outlet />;
+}
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,13 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./layouts/RootLayout";
+import { PostList } from "./pages/PostList";
+import { UserList } from "./pages/UserList";
+import { TodoList } from "./pages/TodoList";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
-        children: [
-            { path: 'posts', element: <PostList /> },
-            { path: 'users', element: <UserList /> },
-            { path: 'todos', element: <TodoList /> },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Navigate to="/posts" /> },
+      { path: "posts", element: <PostList /> },
+      { path: "users", element: <UserList /> },
+      { path: "todos", element: <TodoList /> },
+    ],
+  },
+]);
